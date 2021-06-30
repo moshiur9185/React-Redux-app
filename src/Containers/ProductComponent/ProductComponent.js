10b39e35d2d5fe8ddb0c8a9d0b37a4bcc4e3d76e
moshiur9185 <moshiur9185@gmail.com>
2021-06-30 22:45:38 +0600

    work done

--- src/Containers/ProductComponent/ProductComponent.js
+++ src/Containers/ProductComponent/ProductComponent.js
@@ -8,7 +8,20 @@ const ProductComponent = () => {
     const { id, title, category, price, image } = product;
     return (
       <div style={{paddingRight: "3vw"}} className="four wide column row" key={id}>
-        
+        <Link to={`/product/${id}`}>
+        <div className="ui link cards">
+          <div style={{height: "35rem", width: "20vw"}} className="card">
+            <div className="image">
+              <img className="image-fluid" style={{ height: "20rem", padding: "1rem"}} src={image} alt={title} />
+            </div>
+            <div className="content">
+              <div className="header">{title}</div>
+              <div className="meta price">$ {price}</div>
+              <div className="meta">{category}</div>
+            </div>
+          </div>
+        </div>
+        </Link>
       </div>
     );
   });
